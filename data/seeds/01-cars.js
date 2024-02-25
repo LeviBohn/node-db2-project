1310d26795081e0d3733b9eb9761482807126d8e
Levi Bohn <levibohn@gmail.com>
2024-02-24 23:14:51 -0600

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
     {
-        vin: '11111111111111111',
+        vin: '4Y1SL65848Z411439',
         make: 'toyota',
         model: 'prius',
         mileage: '215000',
@@ -9,7 +9,7 @@ const cars = [
         transmission: 'manual'
     },
     {
-        vin: '22222222222222222',
+        vin: '3Y1SL65848Z411439',
         make: 'toyota',
         model: 'corolla',
         mileage: '115000',
@@ -17,7 +17,7 @@ const cars = [
 
     },
     {
-        vin: '33333333333333333',
+        vin: '2Y1SL65848Z411439',
         make: 'ford',
         model: 'focus',
         mileage: '15000',
