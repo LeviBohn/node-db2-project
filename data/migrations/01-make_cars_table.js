98ad9f3c72a5cfd7daf2bead524cacfffafdb9a1
Levi Bohn <levibohn@gmail.com>
2024-02-24 21:40:02 -0600

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,22 @@
+const { table } = require("console");
+
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 17).notNullable().unique()
+
+    tbl.string('make', 128).notNullable()
+
+    tbl.string('model', 128).notNullable()
+
+    tbl.numeric('mileage').notNullable()
+
+    tbl.string('title', 128).defaultTo('gotta figure this out!')
+
+    tbl.string('transmission', 128).defaultTo('gotta figure this out!')
+  })
 };
 
 exports.down = function (knex) {
