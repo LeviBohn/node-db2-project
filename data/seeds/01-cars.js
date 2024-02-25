762e0cf492d4436a53a672e720e01289d5cdfb7c
Levi Bohn <levibohn@gmail.com>
2024-02-24 21:32:45 -0600

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+        vin: '11111111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: '215000',
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '11111111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: '115000',
+        title: 'salvage',
+
+    },
+    {
+        vin: '11111111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: '15000',
+
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
