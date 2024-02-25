8dc942ef302f1c80c56d9e41fd87ccd9870e9070
Levi Bohn <levibohn@gmail.com>
2024-02-24 23:06:09 -0600

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -3,6 +3,9 @@ const express = require('express')
 const Car = require('./cars-model')
 const {
     checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
 } = require('./cars-middleware')
 
 const router = express.Router()
@@ -25,8 +28,13 @@ router.get('/:id', checkCarId, async (req, res, next) => {
     }
 })
 
-router.post('/', async (req, res, next) => {
-    res.json('posting new car')
-})
+router.post(
+    '/',
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
+    async (req, res, next) => {
+        res.json('posting new car')
+    })
 
 module.exports = router
